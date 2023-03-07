ae54cbc7161011cd740d60443d56ef53c71de4c3
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2023-03-07 13:34:44 +0800

    rollbackc

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,16 @@
 //import adapter from '@sveltejs/adapter-auto';
-import node from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: node(),
+		adapter: adapter(),
 		csrf: {
 			checkOrigin: false
-		},
-		vite: {
-			optimizeDeps: {
-				include: ['svelte-qrCode']
-			}
 		}
 	},
-	preprocess: vitePreprocess(),
-	typescript: {
-		tsconfigFile: './tsconfig.json'
-	}
+	preprocess: vitePreprocess()
 };
 
 export default config;
